356ce9be71448073cc9c385074e2de138b697136
jedolce <juandolce20@gmail.com>
2023-12-24 13:41:42 -0300

    fix client section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,12 +5,11 @@ import { clients } from '../constants'
 const Clients = () => (
     <section className={`${styles.flexCenter} my-4`}>
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
-            {clients.map((client) => {
-                <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[100px] min-w-[120px]`}>
-                    <img src={client.logo} alt="client" className='
-                    sm:w-[192px] w-[100px] object-contain'/>
+            {clients.map((client) => (
+                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+                    <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
                 </div>
-            })}
+            ))}
         </div>
     </section>
 )
