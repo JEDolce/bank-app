e7981837e93e77417b867c8928946f8bac91f11a
jedolce <juandolce20@gmail.com>
2023-12-24 19:37:15 -0300

    add some effects

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
     return (
-        <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+        <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] transition duration-250 ease-out hover:ease-in hover:text-[#5d5a5c] hover:bg-black hover:translate-x-2`}>
             Get Started
         </button>
     )
